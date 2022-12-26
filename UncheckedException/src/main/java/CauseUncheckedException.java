21768a7aab7995fa3b427ba1919e9d4e0be2ea92
ProsperEF <Mprosp2020@gmail.com>
2022-12-26 14:42:08 +0000

    Saved progress on UncheckedException 2022-12-26 14:42:08 +0000

--- UncheckedException/src/main/java/CauseUncheckedException.java
+++ UncheckedException/src/main/java/CauseUncheckedException.java
@@ -14,6 +14,8 @@ public class CauseUncheckedException {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        int a=-20;
+        int b = 0;
+        System.out.println(a%b);
     }
 }
