ea2b39ca5604ab032e49594b7436a6a7a7a0b9d8
ProsperEF <Mprosp2020@gmail.com>
2023-01-19 16:42:58 +0000

    Saved progress on InnerJoin 2023-01-19 16:42:58 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,3 +1,1 @@
-SELECT student.id, student.student_name FROM student INNER JOIN class
-ON class.class_title = student.class_title 
-WHERE class.class_title = 'Physics' AND 'Maths';
+SELECT student.id, student.student_name FROM student INNER JOIN class ON class.class_title = student.class_title WHERE class.teacher_name = 'Ms. Lovelace';
