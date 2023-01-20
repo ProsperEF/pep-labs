34d580938aec052a5629b4670d29eb98d106026e
ProsperEF <Mprosp2020@gmail.com>
2023-01-20 18:51:43 +0000

    Saved progress on Pyramid 2023-01-20 18:51:43 +0000

--- Pyramid/src/main/java/Pyramid.java
+++ Pyramid/src/main/java/Pyramid.java
@@ -27,6 +27,23 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+        char a ='*';
+        String result = "";
+
+        for(int i=0; i<n; i++){
+
+            for(int j=0; j<i+1; j++){
+                
+                a='*';
+                result = result+a;
+            }
+
+            a='\n';
+            result = result+a;
+            
+        }
+
+
+        return result;
     }
 }
