4a48647b398cfa08e3a13f68b0c83d0fa9d1b0bb
ProsperEF <Mprosp2020@gmail.com>
2023-01-23 17:44:34 +0000

    Saved progress on ContainsDuplicates 2023-01-23 17:44:34 +0000

--- ContainsDuplicates/src/main/java/ContainsDuplicates.java
+++ ContainsDuplicates/src/main/java/ContainsDuplicates.java
@@ -1,12 +1,5 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class ContainsDuplicates {
     /**
