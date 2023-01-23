a4ffa8acdd90208363e8cdac0f5a3f04823d7c36
ProsperEF <Mprosp2020@gmail.com>
2023-01-23 17:43:28 +0000

    Saved progress on ContainsDuplicates 2023-01-23 17:43:28 +0000

--- ContainsDuplicates/src/main/java/ContainsDuplicates.java
+++ ContainsDuplicates/src/main/java/ContainsDuplicates.java
@@ -1,4 +1,12 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +16,16 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+       
+       Set <Integer> numSet = new HashSet<>();
+
+
+       for (Integer num : nums){
+        if(!numSet.add(num)){
+            return true;
+        }
+
+       }
         return false;
     }
 }
