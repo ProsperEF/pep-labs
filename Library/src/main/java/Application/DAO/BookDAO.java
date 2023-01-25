e752240c59e917965960ed6d7ecf933ed270637c
ProsperEF <Mprosp2020@gmail.com>
2023-01-25 16:20:01 +0000

    Saved progress on FlightTracker 2023-01-25 16:20:00 +0000

--- Library/src/main/java/Application/DAO/BookDAO.java
+++ Library/src/main/java/Application/DAO/BookDAO.java
@@ -32,7 +32,7 @@ public class BookDAO {
         List<Book> books = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "SELECT * FROM Book;";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
@@ -57,10 +57,12 @@ public class BookDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "SELECT * FROM Book WHERE isbn = ?;";;
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write preparedStatement's setInt method here.
+            preparedStatement.setInt(1, isbn);
+
 
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
@@ -87,10 +89,16 @@ public class BookDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "change me" ;
+            String sql = "INSERT INTO Book (isbn, title, author_id, copies_available ) VALUES (?, ?, ?, ?);" ;
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write preparedStatement's setString and setInt methods here.
+            preparedStatement.setInt(1, book.getIsbn());
+            preparedStatement.setString(2, book.getTitle());
+            preparedStatement.setInt(3, book.getAuthor_id());
+            preparedStatement.setInt(4, book.getCopies_available());
+
+
 
             preparedStatement.executeUpdate();
             return book;
@@ -109,10 +117,12 @@ public class BookDAO {
         List<Book> books = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql =  "SELECT * FROM Book WHERE copies_available > ?;";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write preparedStatement's setInt method here.
+            preparedStatement.setInt(1, 0);
+
 
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
