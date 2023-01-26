89657cf1dddfcacce5ebe12a9cbe0c3b27c5dd90
ProsperEF <Mprosp2020@gmail.com>
2023-01-26 17:32:24 +0000

    Saved progress on WordCount 2023-01-26 17:32:24 +0000

--- WordCount/src/main/java/ReturnWordCount.java
+++ WordCount/src/main/java/ReturnWordCount.java
@@ -5,15 +5,19 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        int count =0;
+        int count =1;
+        int finalCount =0;
         
         for (int i=0; i<in.length(); i++){
-            if (in.charAt(i)==' '){
-                count = 1;
-                count++;
+            if(!(in.isEmpty()) && in.length()==1){
+                finalCount=1;
+            }
+
+            else if (in.charAt(i)==' '){
+                finalCount = 1+count++;
             }
 
         }
-        return count;
+        return finalCount;
     }
 }
