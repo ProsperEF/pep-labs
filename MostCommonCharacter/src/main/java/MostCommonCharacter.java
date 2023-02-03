ee89d6d0367ff3368abcea900ebe32e279088a1a
ProsperEF <Mprosp2020@gmail.com>
2023-02-03 19:13:39 +0000

    Saved progress on MostCommonCharacter 2023-02-03 19:13:39 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MostCommonCharacter {
     /**
@@ -8,22 +12,49 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        int stringLength = str.length();
-        int charCount[] = new int[200];
-        int mostCount = 0;
-        char finalResult =' ';
+        int charCount[]= new int[200];
+        char finalResult = ' ';
 
-        for (int i=0; i<stringLength; i++){
-            charCount[str.charAt(i)]++;
+        char[] orderedChar = str.toCharArray();
+        Arrays.sort(orderedChar);
+        String sortedString = new String(orderedChar);
 
+        List <Integer> charCountList = new ArrayList<Integer>();
+        List <Character> charCountChar = new ArrayList<Character>();
+
+        for (int i=0; i<str.length(); i++){
+            charCount[sortedString.charAt(i)]++;
         }
 
-        for (int i=0; i<stringLength;i++){
-            if(mostCount <charCount[str.charAt(i)]){
-                mostCount = charCount[str.charAt(i)];
-                finalResult = str.charAt(i);
+        for (int i=0; i<str.length(); i++){
+            char check1 = sortedString.charAt(i);
+            char check2 = ' ';
+            int count = i+1;
+
+            if(count < str.length()){
+                check2 = sortedString.charAt(i+1);
+            }
+
+            if (check1 != check2){
+                charCountList.add(charCount[sortedString.charAt(i)]);
+                charCountChar.add(sortedString.charAt(i));
             }
         }
+
+        int max = Collections.max(charCountList);
+        char min = ' ';
+
+        for (int i=0; i<charCountList.size(); i++){
+            if(charCountList.get(i)==max && charCountChar.get(i) <min){
+                min = charCountChar.get(i);
+                finalResult = min;
+            }
+            else{
+                finalResult = charCountChar.get(charCountList.indexOf(max));
+            }
+        }
+
+    
         return finalResult;
     }
 }
